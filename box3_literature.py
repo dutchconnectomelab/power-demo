12d7616206234f8af16d06fecad17057c775a770
Koen Helwegen <kg.helwegen@gmail.com>
2022-11-30 09:19:27 +0100

    more details on literature

--- box3_literature.py
+++ box3_literature.py
@@ -26,6 +26,46 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
 
     df["Mentions power"] = [f(x) for x in df["Mentions power"]]
 
+    def read_samplesize(x):
+        if x == "nan":
+            return np.nan
+        if not type(x) == str:
+            if np.isnan(x):
+                return np.nan
+        assert f"{x}".split(".")[0] == f"{int(x)}"
+        return int(x)
+
+    all_sample_sizes = []
+    total_subjects = []
+    for _, row in df.iterrows():
+        sample_sizes = np.array(
+            [read_samplesize(row[f"Sample {i}"]) for i in range(1, 6)]
+        )
+        try:
+            sample_sizes = sample_sizes[~np.isnan(sample_sizes)]
+            total_subjects.append(np.sum(sample_sizes))
+            if len(sample_sizes) == 1:
+                sample_sizes = [sample_sizes[0]]
+            else:
+                sample_sizes = sorted(sample_sizes)[-2:]
+            sample_sizes = np.array(sample_sizes, dtype=np.int16)
+        except TypeError:
+            raise RuntimeError(f"Could not process sample sizes: {sample_sizes}")
+
+        all_sample_sizes.append(sample_sizes)
+
+    assert len(all_sample_sizes) == len(df)
+
+    df["sample sizes"] = all_sample_sizes
+    df["total subjects"] = total_subjects
+    df["mean sample size"] = [np.mean(x) for x in all_sample_sizes]
+
+    # only keep studies with at least two samples
+    print(
+        f"Removing {np.sum([len(x) == 1 for x in all_sample_sizes])} that only have one sample"
+    )
+    df = df[[len(x) > 1 for x in all_sample_sizes]]
+
     return df
 
 
@@ -70,29 +110,8 @@ def load_litreview() -> pd.DataFrame:
 def assess_power(df: pd.DataFrame, alpha: float, effect_size: float, name=""):
     power = []
 
-    def read_samplesize(x):
-        if x == "nan":
-            return np.nan
-        if not type(x) == str:
-            if np.isnan(x):
-                return np.nan
-        assert f"{x}".split(".")[0] == f"{int(x)}"
-        return int(x)
-
     for _, row in df.iterrows():
-        sample_sizes = np.array(
-            [read_samplesize(row[f"Sample {i}"]) for i in range(1, 6)]
-        )
-        try:
-            sample_sizes = sample_sizes[~np.isnan(sample_sizes)]
-            if len(sample_sizes) == 1:
-                print("SKIPPING")
-                continue
-                sample_sizes = [sample_sizes[0], sample_sizes[0]]
-            sample_sizes = sorted(sample_sizes)[-2:]
-            sample_sizes = np.array(sample_sizes, dtype=np.int16)
-        except TypeError:
-            raise RuntimeError(f"Could not process sample sizes: {sample_sizes}")
+        sample_sizes = row["sample sizes"]
 
         power.append(
             tt_ind_solve_power(
@@ -127,17 +146,47 @@ def power_violinplot(df):
     ax.set(ylim=(0, 1), yticks=(0, 0.8, 1))
     ax.axhline(0.8, ls="--", color="red")
     plt.savefig(fn := Path("RESULTS") / "power_in_literature_violinplot.svg")
-    print(f"Saved figure to {fn }")
-    plt.show()
+    print(f"Saved figure to {fn}")
+    # plt.show()
+
+
+def samplesize_plot(df: pd.DataFrame):
+    for m in ["sc", "fc"]:
+        x = np.concatenate(df[df["modality"] == m]["sample sizes"].to_list())
+        x = x[x < 300]
+        ax = sns.histplot(x, stat="percent", bins=50)
+        plt.ylim(0, 20)
+        # plt.xlim(0, 200)
+        # plt.savefig(
+        #     fn := Path("RESULTS") / f"{m}_sample_sizes_in_literature_histogram.svg"
+        # )
+        # print(f"Saved figure to {fn}")
+        plt.show()
+        plt.clf()
+
+
+def sample_size_report(df: pd.DataFrame, name: str):
+    print(f"\nSAMPLE SIZE REPORT FOR {name.upper()}")
+    x = np.concatenate(df["sample sizes"].to_list())
+    assert np.ndim(x) == 1
+    print(f"Median: {np.median(x):.0f}")
+    print(
+        f"10th - 90th percentile: {np.percentile(x, 10):.0f} - {np.percentile(x, 90):.0f}"
+    )
 
 
 def power_discussion(df):
     print("\n## POWER DISCUSSIONS ##")
     n_disc = np.sum(df["Mentions power"])
     print(f"Power discussed in {n_disc:.0f} out of {len(df)} papers")
-    print(
-        f"Power analysis included in {np.sum(df['Power analysis']):.0f} out of {len(df)} papers"
-    )
+    try:
+        print(
+            f"Power analysis included in {np.sum(df['Power analysis']):.0f} out of {len(df)} papers"
+        )
+    except TypeError:
+        raise ValueError(
+            f'Could not process some power analysis labels out of: {df["Power analysis"].unique()}'
+        )
 
     def clean_lbl(lbl: str) -> str:
         lbl = lbl.strip().lower()
@@ -188,14 +237,25 @@ def power_discussion(df):
 if __name__ == "__main__":
     df = load_litreview()
 
+    samplesize_plot(df)
+
     print("\n## TOTAL INCLUDED ##")
     for m in ["sc", "fc"]:
         print(f"{m.upper()}: {np.sum(df['modality'] == m)}")
 
+    sample_size_report(df, "all")
+    for m in ["sc", "fc"]:
+        sample_size_report(df[df["modality"] == m], m)
+
     print("\n## POWER ESTIMATES ##")
+
+    assess_power(df, 0.05, 0.8, "strong effects (overall)")
+    assess_power(df, 0.05 / 50, 0.8, "strong effects (overall)")
+
     resdf = pd.DataFrame()
     for m in ["sc", "fc"]:
-        for e, a in zip([0.2, 0.8], [0.05, 0.001]):
+        print("----------------------------------")
+        for e, a in zip([0.2, 0.8, 0.8, 0.8], [0.05, 0.05, 0.001, 0.05 / 1225]):
             df_ = assess_power(df[df["modality"] == m], a, e, m.upper())
             resdf = pd.concat([resdf, df_])
 
